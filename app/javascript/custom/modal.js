065f2c57f2ed37e0923053769d7eff094b8fb900
itman1000 <itman202043@gmail.com>
2022-10-05 21:15:45 +0900

    削除確認画面作成２

--- app/javascript/custom/modal.js
+++ app/javascript/custom/modal.js
@@ -4,6 +4,8 @@
   const modal = document.getElementById('modal');
   const mask = document.getElementById('mask');
   const eliminate = document.getElementById('eliminate');
+  const del = document.getElementById('del');
+  const cancel = document.getElementById('cancel');
 
   console.log(eliminate);
   
@@ -13,4 +15,11 @@
       mask.classList.remove('hidden2');
     });
   });
+
+  cancel.addEventListener("click", () => {
+    modal.classList.add('hidden2');
+    mask.classList.add('hidden2');
+  });
+  
+
 }
